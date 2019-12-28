a44eb7c6c4d777b7feb03da0c260ad6786102325
Ren Lucas <llucas314@gmail.com>
2019-12-28 09:39:13 -0500

    added icons

--- src/Icons/index.js
+++ src/Icons/index.js
@@ -1,2 +1,5 @@
 export { default as BorderDesign } from './BorderDesign'
 export { default as ButtonIcon } from './ButtonIcon'
+export { default as Desktop } from './Desktop'
+export { default as Mobile } from './Mobile'
+export { default as Website } from './Website'
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./About.css";
 import image from "../../assets/GA14Nov-WebSized_NoCrop-11.jpg";
 import { BorderDesign } from "../../Icons";
-import borderDesign from "../../assets/borderDesign.svg";
+import borderDesign from "../../assets/backgroundImage.png";
 export default function About() {
   return (
     <div
@@ -10,7 +10,7 @@ export default function About() {
       className="about d-flex flex-lg-row justify-content-center align-items-center"
     >
       <div className="container">
-        <div className="row flex-column justify-content-center align-items-center">
+        <main className="row flex-column justify-content-center align-items-center about__main">
           <h1 className="about__h1 text-center">ABOUT ME</h1>
           <div className="about__bio d-flex flex-column flex-lg-row justify-content-center align-items-center col-lg-12">
             <img
@@ -18,21 +18,20 @@ export default function About() {
               alt="profile"
               className="about__image rounded-circle p-3"
             />
-            <p className="about__bio-text text-center">
+            <p className="about__bio-text text-center mb-3">
               Give it up for my sister! All right now. Everybody put your hands
               together. Do we have any single ladies in the house tonight? sing
               All the single ladies All the single ladies. Now put your hands
               up. Up in the club. Just broke up. I’m doing my own littl
             </p>
           </div>
-        </div>
+        </main>
       </div>
       <div
         className="about__footer "
-        // style={{ backgroundImage: `url(${borderDesign})` }}
+        style={{ backgroundImage: `url(${borderDesign})` }}
       >
-        <BorderDesign width={"100vw"} height={"auto"} />
-        <h3 className="about__h3">What I Do</h3>
+        <h3 className="about__h3 text-center">WHAT I DO</h3>
         <div className="about__icons"></div>
       </div>
     </div>
