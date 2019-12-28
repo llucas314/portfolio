5d7549871f8f5a36567fda00373ba7bfc8b8a36b
Ren Lucas <llucas314@gmail.com>
2019-12-28 18:02:42 -0500

    added all sites

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -7,10 +7,10 @@ import { Container, Row } from "reactstrap";
 export default function Project(props) {
   return (
     <div
-      className="project col-12 col-lg-4 p-0"
+      className="project  col-lg-4 p-0"
       style={{ backgroundImage: `url(${props.img})` }}
     >
-      <div className="project__modal  p-0 col-12  d-flex flex-column justify-content-center align-items-center">
+      <div className="project__modal  p-0  d-flex flex-column justify-content-center align-items-center">
         <h1 className="project__h1">{props.title}</h1>
         <div className="project__links d-flex justify-content-center align-items-center">
           <a
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -2,25 +2,36 @@ import React from "react";
 import "./Portfolio.css";
 import Project from "../../components/Project/Project";
 import calluna from "../../assets/calluna.png";
+import cove from "../../assets/music-cove.png";
+import superBowl from "../../assets/super-bowl.png";
+import doggy from "../../assets/doggy-styles.png";
+import simon from "../../assets/simon.png";
+import national from "../../assets/parks.png";
 import { Container, Row } from "reactstrap";
 
 export default function Portfolio() {
   return (
-    <div className="portfolio" id="portfolio">
-      <Container>
-        <Row>
+    <div
+      className="portfolio d-flex flex-column justify-content-center align-items-center"
+      id="portfolio"
+    >
+      <div className="portfolio__h1">PORTFOLIO</div>
+      <div className="container">
+        <div className="row">
           <div className=" col-12 d-flex flex-column flex-lg-row">
-            <Project
-              title="THE CALLUNA"
-              img={calluna}
-              link="https://www.google.com"
-            />
-            <Project title="calluna" img={calluna} />
-            <Project />
+            <Project title="The Calluna" img={calluna} />
+            <Project title="The Music Cove" img={cove} />
+            <Project title="Super Bowl Center" img={superBowl} />
           </div>
-        </Row>
-        <Row></Row>
-      </Container>
+        </div>
+        <div className="row">
+          <div className=" col-12 d-flex flex-column flex-lg-row">
+            <Project title="Simon" img={simon} />
+            <Project title="Doggy Styles" img={doggy} />
+            <Project title="National Parks" img={national} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
