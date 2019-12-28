9fd2aebf35aad352e5bc1f8dd404361cfbeab6ce
Ren Lucas <llucas314@gmail.com>
2019-12-28 16:07:23 -0500

    got project ratios right

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -6,44 +6,28 @@ import { Container, Row } from "reactstrap";
 
 export default function Project(props) {
   return (
-    <Container>
-      <Row>
-        <div
-          className="project col-12 p-0"
-          style={{ backgroundImage: `url(${props.img})` }}
-        >
-          <div className="project__modal  p-0 col-12 d-flex flex-column justify-content-center align-items-center">
-            <h1 className="project__h1">{props.title}</h1>
-            <div className="project__links d-flex justify-content-center align-items-center">
-              {props.link ? (
-                <a
-                  href={props.link}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  className="project__link m-2"
-                >
-                  <DarkArrow width={"32px"} height={"32px"} />
-                </a>
-              ) : (
-                ""
-              )}
-              <a
-                href={props.github}
-                rel="noopener noreferrer"
-                target="_blank"
-                className="project__link m-2"
-              >
-                <GithubBrands width={"32px"} height={"32px"} />
-              </a>
-            </div>
-          </div>
+    <div
+      className="project col-12 col-lg-4 p-0"
+      style={{ backgroundImage: `url(${props.img})` }}
+    >
+      <div className="project__modal  p-0 col-12  d-flex flex-column justify-content-center align-items-center">
+        <h1 className="project__h1">{props.title}</h1>
+        <div className="project__links d-flex justify-content-center align-items-center">
+          <a
+            href={props.link}
+            rel="noopener noreferrer"
+            target="_blank"
+            className="project__link m-2"
+          >
+            <DarkArrow width={"32px"} height={"32px"} />
+          </a>
         </div>
-      </Row>
-    </Container>
+      </div>
+    </div>
   );
 }
 Project.defaultProps = {
   img: "/",
   title: "GOOGLE",
-  github: "https://www.google.com"
+  link: "https://www.google.com"
 };
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -2,21 +2,25 @@ import React from "react";
 import "./Portfolio.css";
 import Project from "../../components/Project/Project";
 import calluna from "../../assets/calluna.png";
+import { Container, Row } from "reactstrap";
 
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      <div className="container">
-        <div className="row">
-          <Project
-            title="THE CALLUNA"
-            img={calluna}
-            link="https://www.google.com"
-          />
-          <Project title="calluna" img={calluna} />
-          <Project />
-        </div>
-      </div>
+      <Container>
+        <Row>
+          <div className=" col-12 d-flex flex-column flex-lg-row">
+            <Project
+              title="THE CALLUNA"
+              img={calluna}
+              link="https://www.google.com"
+            />
+            <Project title="calluna" img={calluna} />
+            <Project />
+          </div>
+        </Row>
+        <Row></Row>
+      </Container>
     </div>
   );
 }
